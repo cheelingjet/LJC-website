29caddfecf65f889b79131df9cab722d91393b09
cheelingjet <lchee@purdue.edu>
2024-05-24 14:12:47 -0700

    resume updates, production build

--- lj-web/src/components/about/About.jsx
+++ lj-web/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../assets/about2.jpg"
-import RESUME from "../../assets/LJC_2024_Resume.pdf"
+import RESUME from "../../assets/LJC_2024_Resume postgrad.pdf"
 import Info from './Info';
 
 const About = () => {
